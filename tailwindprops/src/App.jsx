cddb031a59a2989076e4fc7e29c00f89495f37f8
Pawan sharma <pawansh803@gmail.com>
2026-01-18 22:45:50 +0530

    react js

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Card from "./commponents/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind CSS</h1>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">Tailwind CSS</h1>
+      <Card username="Peter" btnText="visit me" />
+      <Card username="Mandy" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
