a1d432cd991b2cb6af97bb5405e34e11dfbe34e8
Pawan sharma <pawansh803@gmail.com>
2026-01-31 23:06:12 +0530

    Currency Convertor

---
+++ currencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-green-500 text-4xl">Currency Convertor</h1>
+    </>
+  );
+}
+
+export default App;
