ac4046c2604b2514f69de211bf925b29f026560d
Pawan sharma <pawansh803@gmail.com>
2026-04-19 17:02:55 +0530

    Todo app with context api and local storage

---
+++ localTodoContext/src/components/TodoForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useTodo } from "../contexts";
+
+function TodoForm() {
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
+
+  const add = (e) => {
+    e.preventDefault();
+
+    if (!todo) return;
+
+    // addTodo({todo: todo, completed: false})
+    // if the field name and value are same then we can write it as below also
+    addTodo({ todo, completed: false });
+    setTodo("");
+  };
+
+  return (
+    <form onSubmit={add} className="flex">
+      <input
+        type="text"
+        placeholder="Write Todo..."
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        onChange={(e) => setTodo(e.target.value)}
+        value={todo}
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
